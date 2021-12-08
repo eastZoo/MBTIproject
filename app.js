586b59cc4fd961c541c9c18a717da53ebc4ea494
LAPTOP-JMJ6A32F\pc <jiny7157502@naver.com>
2021-12-08 22:09:34 +0900

    develop/jini branch 생성

--- app.js
+++ app.js
@@ -31,7 +31,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/board', boardRouter);
 app.use('/user', userRouter);
 
-
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
